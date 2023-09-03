3c4c39b9375e3951316de6e2a1e45daa73beb28e
LucasHJesus <lukaslima641@gmail.com>
2023-09-03 14:40:15 -0300

    tarefa 1

---
+++ 1a/sim/mat1_3.m
@@ -0,0 +1,10 @@
+%Lucas Humberto Jesus de Lima
+%12011ECP011
+
+%% Parametros
+m = 3;
+k = 20;
+b = 1.5;
+g = 9.81;
+
+sim('simu1_3.slx')
---
+++ 1a/sim/mat1_4.m
@@ -0,0 +1,9 @@
+%Lucas Humberto Jesus de Lima
+%12011ECP011
+
+%% Parametros
+R = 1;
+L = 1;
+C = 1;
+
+sim('simu1_4.slx')
